6dc57a8ea52b6405b03e04a216e7ce95a099565e
Rutvik <rutvikjani33@gmail.com>
2026-03-24 05:41:34 +0530

    Fixed a Test Case

--- src/pos/test/OrderWriterTest.java
+++ src/pos/test/OrderWriterTest.java
@@ -11,7 +11,7 @@ public class OrderWriterTest {
 
     @Test
     public void testOrderIsWrittenToCSV() throws Exception {
-        FileWriter writer = new FileWriter("Coffe_Shop_Simulator/existing_orders.csv", true);
+        FileWriter writer = new FileWriter("existing_orders.csv", true);
         writer.write("99,99,12:00:00,\"BEV-001,FOOD-001\",9.00,0.45,8.55\n");
         writer.close();
 
@@ -22,7 +22,7 @@ public class OrderWriterTest {
 
     @Test
     public void testWrittenOrderCustomerId() throws Exception {
-        FileWriter writer = new FileWriter("Coffe_Shop_Simulator/existing_orders.csv", true);
+        FileWriter writer = new FileWriter("existing_orders.csv", true);
         writer.write("98,98,11:00:00,\"BEV-002,FOOD-002\",5.90,0.30,5.60\n");
         writer.close();
 
@@ -36,7 +36,7 @@ public class OrderWriterTest {
 
     @Test
     public void testWrittenOrderTimestamp() throws Exception {
-        FileWriter writer = new FileWriter("Coffe_Shop_Simulator/existing_orders.csv", true);
+        FileWriter writer = new FileWriter("existing_orders.csv", true);
         writer.write("97,97,10:00:00,OTH-001,3.00,0.00,3.00\n");
         writer.close();
 
@@ -50,8 +50,9 @@ public class OrderWriterTest {
 
     @Test
     public void testWrittenOrderItemIds() throws Exception {
-        FileWriter writer = new FileWriter("Coffe_Shop_Simulator/existing_orders.csv", true);
+        FileWriter writer = new FileWriter("existing_orders.csv", true);
         writer.write("96,96,09:00:00,BEV-003\n");
+        writer.close();
         List<ExistingOrder> orders = OrderLoader.loadOrders();
         ExistingOrder found = orders.stream()
                 .filter(o -> o.getOrderNo().equals("96"))
@@ -62,7 +63,7 @@ public class OrderWriterTest {
 
     @Test
     public void testWrittenOrderNumber() throws Exception {
-        FileWriter writer = new FileWriter("Coffe_Shop_Simulator/existing_orders.csv", true);
+        FileWriter writer = new FileWriter("existing_orders.csv", true);
         writer.write("95,95,08:00:00,FOOD-004,4.00,0.00,4.00\n");
         writer.close();
 
